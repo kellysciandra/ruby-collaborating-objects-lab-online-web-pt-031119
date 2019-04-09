a6d679fad7609c83e96bbe99ef6b31f6e65b6328
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-09 21:20:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,6 +33,12 @@ class Artist
     def self.find_by_name(name)
       @@all.find{|song| song.name == name}
     end
+    
+    def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
   
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) 
