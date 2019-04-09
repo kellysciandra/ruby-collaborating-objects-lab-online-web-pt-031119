63be7db31a2c70b349b31c346216080557538db9
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-09 20:10:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,13 @@ class Song
     song
   end
   
-
+ def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
   
   
 end 
