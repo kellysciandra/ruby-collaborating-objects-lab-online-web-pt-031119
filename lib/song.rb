342d3afa16a43aa59609ae527d2b4d292a5df35d
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-09 21:18:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,6 @@ class Song
   def self.new_by_filename(file_name)
       self.name = file_name.split(" - ")[0]
       self.artist.name = file_name.split(" - ")[1]
-  end
 end
   
  def artist_name=(name)
