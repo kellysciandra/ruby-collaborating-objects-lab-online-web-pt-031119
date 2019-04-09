8a4cb016c0f4612121eebdbcc4cd3e2d91e59f3d
Kelly Sciandra <kellysciandradev@icloud.com>
2019-04-09 21:13:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,13 @@
 require 'pry'
 class Artist 
   
-  attr_accessor :name, :songs, :song 
+  attr_accessor :name, :songs
   @@all = []
+  @songs= []
   
   def initialize(name)
     @name = name 
-    @songs= []
+    
   end 
   
 
